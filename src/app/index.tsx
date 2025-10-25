d0498772297c70bf2c8f828344c9b1a6bf673807
yRizer <taynan28112003@gmail.com>
2025-10-25 17:20:48 -0300

    Finalizado botões

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import Button from '../components/buttons';
 import { rootColors, rootTexts } from '../styles/styles';
 
+const logo = require('images/logo/logo.png');
+
 export default function App() {
-  return (
-    <View style={styles.mainContainer}>
-        <View style={styles.logoConatiner}>
-            {/* Logo vai aqui */}
-            <Text style={[rootTexts.title, {marginBottom: 20}]}>Bem Vindo</Text>
-            <Text style={[rootTexts.text, {textAlign: 'center'}]}>Explore os encantos de Espirito Santo do Pinhal</Text>
+    return (
+        <View style={styles.mainContainer}>
+            <View style={styles.logoConatiner}>
+                {/* Logo vai aqui */}
+                <Image source={logo} style={{ width: 'auto', height: 75, aspectRatio: 1, marginBottom: 20}} resizeMode='cover' />
+                <Text style={[rootTexts.title, { marginBottom: 20 }]}>Bem Vindo</Text>
+                <Text style={[rootTexts.text, { textAlign: 'center' }]}>Explore os encantos de Espirito Santo do Pinhal</Text>
+            </View>
+            <View style={styles.bottomContainer}>
+                <Button text="Fazer Login" leftIcon='person-circle' rightIcon='arrow-forward' />
+                <Button text="Escanear QR Code" backgroundColor={rootColors.branco} textColor={rootColors.marrom} outLine={{ borderWidth: 3, borderColor: rootColors.marrom }} leftIcon='qr-code' rightIcon='arrow-forward' />
+                <Button text="Explorar sem conta" backgroundColor={rootColors.branco} textColor={rootColors.marrom} outLine={{ borderWidth: 1, borderColor: rootColors.marrom }} rightIcon='arrow-forward' />
+            </View>
         </View>
-        <View style={styles.bottomContainer}>
-            <Button text="Escanear QR Code"/>
-        </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -25,7 +30,7 @@ const styles = StyleSheet.create({
         backgroundColor: rootColors.background,
     },
     logoConatiner: {
-        flex: 1,
+        flex: 2,
         marginTop: 60,
         maxWidth: '60%',
         alignSelf: 'center',
@@ -35,9 +40,11 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     bottomContainer: {
+        gap: 40,
         flex: 3,
-        width: '100%',
         borderRadius: 24,
+        paddingInline: 40,
+        width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: rootColors.branco,
