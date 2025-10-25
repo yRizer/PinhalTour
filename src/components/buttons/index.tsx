ce679524862e878ab82261cca19bbc2b4bbd7bce
yRizer <taynan28112003@gmail.com>
2025-10-25 17:21:23 -0300

    Adaptado e estilizado para suportar personalizações

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -1,14 +1,37 @@
-import { Pressable, Text } from "react-native";
+import { rootColors } from "@/src/styles/styles";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { DimensionValue, Pressable, Text } from "react-native";
 import { styles } from "./styles";
 
 type ButtonProps = {
     text: string;
+    textColor?: string;
+    width?: DimensionValue;
+    outLine?: { borderWidth: number, borderColor: string };
+    backgroundColor?: string;
+    leftIcon?: keyof typeof Ionicons.glyphMap;
+    rightIcon?: keyof typeof Ionicons.glyphMap;
+    onPress?: () => void;
 }
 
-export default function Button({ text }: ButtonProps) {
+export default function Button({ text, width = '100%', outLine, backgroundColor, textColor, onPress, leftIcon, rightIcon }: ButtonProps) {
     return (
-        <Pressable style={[styles.buttonContainer]}>
-            <Text style={styles.textButton}>{text}</Text>
+        <Pressable
+            style={[styles.buttonContainer,
+            {
+                width: width,
+                backgroundColor: backgroundColor ? backgroundColor : rootColors.vinho,
+                borderWidth: outLine?.borderWidth,
+                borderColor: outLine?.borderColor,
+            }
+            ]}
+            onPress={onPress}>
+
+            <Text style={[styles.textButton, { color: textColor ? textColor : rootColors.branco }]}>{text}</Text>
+
+            {leftIcon && <Ionicons name={leftIcon} size={30} color={textColor ? textColor : rootColors.branco} style={{ position: 'absolute', left: 30 }} />}
+            {rightIcon && <Ionicons name={rightIcon} size={30} color={textColor ? textColor : rootColors.branco} style={{ position: 'absolute', right: 30 }} />}
+            
         </Pressable>
     );
 }
--- src/components/buttons/styles.ts
+++ src/components/buttons/styles.ts
@@ -1,15 +1,14 @@
-import { rootColors } from "@/src/styles/styles";
 
 export const styles = {
     buttonContainer: {
         padding: 25,
-        backgroundColor: rootColors.vinho,
         borderRadius: 20,
         alignItems: "center" as const,
+        justifyContent: "center" as const,
+        boxSizing: 'border-box'
     },
     textButton: {
-        color: rootColors.branco,
         fontSize: 24,
-        fontWeight: "400" as const,
+        fontWeight: "500" as const,
     }
 }
