d15aeafa32ac4d5372bd122f01c12babe9756e81
yRizer <taynan28112003@gmail.com>
2025-10-25 15:53:07 -0300

    Adicionado boas vindas e testando botão

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from '../components/buttons';
 import { rootColors, rootTexts } from '../styles/styles';
 
 export default function App() {
@@ -7,9 +8,12 @@ export default function App() {
     <View style={styles.mainContainer}>
         <View style={styles.logoConatiner}>
             {/* Logo vai aqui */}
-            <Text style={rootTexts.title}>Bem Vindo</Text>
+            <Text style={[rootTexts.title, {marginBottom: 20}]}>Bem Vindo</Text>
+            <Text style={[rootTexts.text, {textAlign: 'center'}]}>Explore os encantos de Espirito Santo do Pinhal</Text>
         </View>
-        <View style={styles.bottomContainer}></View>
+        <View style={styles.bottomContainer}>
+            <Button text="Escanear QR Code"/>
+        </View>
     </View>
   );
 }
@@ -17,21 +21,25 @@ export default function App() {
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
+        position: 'relative',
         backgroundColor: rootColors.background,
-        position: 'relative'
     },
     logoConatiner: {
+        flex: 1,
         marginTop: 60,
+        maxWidth: '60%',
+        alignSelf: 'center',
+        textAlign: 'center',
         alignItems: 'center',
-        justifyContent: 'center',
         flexDirection: 'column',
+        justifyContent: 'center',
     },
     bottomContainer: {
-        bottom: 0,
-        borderRadius: 24,
-        height: '60%',
+        flex: 3,
         width: '100%',
-        position: 'absolute',
+        borderRadius: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: rootColors.branco,
         boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
     }
