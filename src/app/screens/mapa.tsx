3c7ea2a1b031d124bf5ff3099be02d6cd4259685
yRizer <taynan28112003@gmail.com>
2025-10-30 14:52:21 -0300

    Adiciona categorias, bottomSheets, animação e interação nas categorias do mapa

--- src/app/screens/mapa.tsx
+++ src/app/screens/mapa.tsx
@@ -1,15 +1,31 @@
 // app/index.js (ou qualquer tela sua)
-import { rootStyles, rootTexts } from '@/src/styles/styles';
+import { SheetUp } from '@/src/components/bottomSheets';
+import MainTextInput from '@/src/components/textInput';
+import { rootColors, rootStyles, rootTexts } from '@/src/styles/styles';
+import { Ionicons } from '@expo/vector-icons';
 import { useFocusEffect } from 'expo-router'; // ou '@react-navigation/native'
-import React, { useCallback } from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useEffect } from 'react';
+import { Pressable, ScrollView, StyleSheet, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Animated, {
+  interpolateColor,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
 
+const dataCategories: { id: string; name: string; icon: keyof typeof Ionicons.glyphMap }[] = [
+  { id: '1', name: 'Históricos', icon: 'hourglass-outline' },
+  { id: '2', name: 'Naturais', icon: 'leaf-outline' },
+  { id: '3', name: 'Culturais', icon: 'library-outline' },
+  { id: '4', name: 'Gastronomia', icon: 'cafe-outline' },
+  { id: '5', name: 'Aventura', icon: 'bicycle-outline' },
+];
+
 export default function MapaScreen() {
+
+  const [selectedCategories, setSelectedCategories] = React.useState<string[]>([]);
+
   // 1. Crie um valor compartilhado para a animação
   const opacity = useSharedValue(0);
 
@@ -34,11 +50,114 @@ export default function MapaScreen() {
     }, [])
   );
 
+  function categoryItem({ item }: { item: { id: string; name: string; icon: keyof typeof Ionicons.glyphMap } }) {
+    const AnimatedIcon = Animated.createAnimatedComponent(Ionicons);
+
+    var backGroundColor = useSharedValue(selectedCategories.includes(item.id) ? rootColors.vinho : rootColors.branco);
+    var color = useSharedValue(selectedCategories.includes(item.id) ? rootColors.branco : rootColors.marrom);
+
+    var iconColor = useSharedValue(0)
+    
+    useEffect(() => {
+      backGroundColor.value = withTiming(selectedCategories.includes(item.id) ? rootColors.vinho : rootColors.branco, { duration: 100 });
+      color.value = withTiming(selectedCategories.includes(item.id) ? rootColors.branco : rootColors.marrom, { duration: 100 });
+      iconColor.value = withTiming(selectedCategories.includes(item.id) ? 1 : 0, { duration: 100 });
+    }, [selectedCategories]);
+
+    var animatedBackground = useAnimatedStyle(() => {
+      return {
+        backgroundColor: backGroundColor.value,
+      };
+    });
+
+    var animatedText = useAnimatedStyle(() => {
+      return {
+        color: color.value,
+      };
+    });
+
+    var animatedIconColor = useAnimatedStyle(() => {
+      return {
+        color: interpolateColor(
+          iconColor.value,
+          [0, 1],
+          [rootColors.marrom, rootColors.branco]
+        ),
+      };
+    });
+
+    return (
+      <Pressable key={item.id} onPress={function () { handleCategoryPress(item.id) }}>
+        <Animated.View style={[styles.categoryItem, animatedBackground]}>
+          <Animated.Text style={[rootTexts.auxiliary, animatedText]}>{item.name}</Animated.Text>
+          <AnimatedIcon name={item.icon} size={24} style={animatedIconColor} />
+        </Animated.View>
+      </Pressable>
+    );
+  }
+
+  function handleCategoryPress(categoryId: string) {
+
+    setSelectedCategories((prevSelected) => {
+      if (prevSelected.includes(categoryId)) {
+        // Remove a categoria se já estiver selecionada
+        return prevSelected.filter((id) => id !== categoryId);
+      } else {
+        // Adiciona a categoria se não estiver selecionada
+        return [...prevSelected, categoryId];
+      }
+    });
+  }
+
   return (
-    <View style={[rootStyles.container, { justifyContent: 'center', alignItems: 'center' }]}>
-      <Animated.Text style={[rootTexts.title, animatedStyle]}>
+    <GestureHandlerRootView style={[rootStyles.container, { justifyContent: 'center', alignItems: 'center', backgroundColor: '#222222ff' }]}>
+      <View style={styles.mapHeader}>
+        <MainTextInput placeholder='Pesquise algum lugar...' />
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.categoriesContainer}>
+          {dataCategories.map(((item) => (
+            categoryItem({ item })
+          )))}
+        </ScrollView>
+      </View>
+
+      <Animated.Text style={[rootTexts.title, animatedStyle, { color: rootColors.branco }]}>
         Mapa
       </Animated.Text>
-    </View>
+
+      <SheetUp
+        SetPosY={75}
+        SheetHeight={1000}
+        Percentage={true}
+        description="Arraste para cima para ver mais opções"
+      />
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  mapHeader: {
+    position: 'absolute',
+    top: 25,
+    width: '90%',
+    height: 'auto',
+  },
+  inputSearch: {
+    padding: 15,
+    height: 'auto',
+  },
+  categoriesContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    gap: 10,
+    overflow: 'visible',
+  },
+  categoryItem: {
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 30,
+    gap: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
